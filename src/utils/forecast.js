70988d98182d52efd9c62eac646b697c6036be38
unknown <mw.schoeler@gmail.com>
2022-03-01 11:58:20 +0800

    added new data to print

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -19,7 +19,9 @@ const forecast = (latitude, longitude, callback) => {
           ". It is currently " +
           body.current.temperature +
           " and it feels like " +
-          body.current.feelslike
+          body.current.feelslike +
+          ". Date and Time: " +
+          body.location.localtime
       );
     }
   });
