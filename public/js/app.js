eab91b416a1d43f94a2e48f08243566710893808
unknown <mw.schoeler@gmail.com>
2022-03-01 10:46:41 +0800

    Removed console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("CliengSide JS file Loaded");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
